6ee8d929c52b1a939ec2367ac525756b6d906dd7
BrunoIIan <bru.ortolan@hotmail.com>
2022-12-21 19:25:13 -0300

    V1.0 - Tela de Login

---
+++ src/services/FirebaseConnect.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADLlgIrbldPU7ZqUdWghqstGtQn2CAXKs",
+  authDomain: "atitusgamer-9e2fc.firebaseapp.com",
+  projectId: "atitusgamer-9e2fc",
+  storageBucket: "atitusgamer-9e2fc.appspot.com",
+  messagingSenderId: "371572806178",
+  appId: "1:371572806178:web:6cfb886bb526dce03e707b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+//export const getAuth = getAuth(app)
